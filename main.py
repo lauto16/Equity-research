8c6a9baec63da07adf323ac4ae1df4d22759a213
lauto16 <lautymartino658@gmail.com>
2024-09-10 20:18:08 -0300

    urls

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def mainRun():
     """
     Main function called when button is pressed on excel file to refresh the data
     """
-
+    
     time.sleep(1)
     print("its working")
     time.sleep(10)
