a2f146a29e3fc24a13b3a4276c843b5737e121e2
Lautaro Martino <73772193+lauto16@users.noreply.github.com>
2024-09-10 21:25:51 -0300

    Changes

--- add_stock.py
+++ add_stock.py
@@ -6,7 +6,15 @@ from time import sleep
 def main(new_symbol, file_name):
     try:
 
-        sleep(2)
+        """
+        Do this when script is ready
+        value = input(
+            "To execute this action you need to close Excel, once you close it, type '1' and enter")
+        while value != '1':
+            value = input(
+                "To execute this action you need to close Excel, once you close it, type '1' and enter")
+
+        """
 
         excel_file = pd.ExcelFile(file_name)
 
