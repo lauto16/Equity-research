6144874b2956eed809f492b25571fa1f1802de60
Koch73 <144285829+Koch73@users.noreply.github.com>
2024-09-14 18:56:12 -0300

    Add files via upload

--- scraper.py
+++ scraper.py
@@ -5,8 +5,8 @@ from formating_tools import clear_number
 from urllib3.exceptions import MaxRetryError
 
 
-def filter_revenue_TTM(driver) -> dict:
-    "Revenue TTM"
+def filter_revenue_TTM(driver, symbol: str, stock_name: str) -> dict:
+    # Revenue TTM
     try:
         # Getting the full html
         html = driver.page_source
@@ -43,7 +43,7 @@ def filter_revenue_TTM(driver) -> dict:
         pass
 
 
-def filter_operating_expenses(driver) -> dict:
+def filter_operating_expenses(driver, symbol: str, stock_name: str) -> dict:
     # Expenses TTM;
 
     try:
@@ -82,102 +82,129 @@ def filter_operating_expenses(driver) -> dict:
         pass
 
 
-def filter_net_income():
+def filter_net_income(driver, symbol: str, stock_name: str) -> dict:
     # Net Income TTM
-    cells_netIncome = []
-    cells_dates = []
+    try:
 
-    with open('Debug/debug1.txt', 'r') as htmlfile:
-        html = htmlfile.read()
+        # Getting the full html
+        html = driver.page_source
 
-    soup = BeautifulSoup(html, 'html.parser')
-    Net_income = soup.find("div", id="row15jqxgrid").children
-    for cell in Net_income:
-        cell = cell.text
-        if cell:
-            cells_netIncome.append(cell)
+        soup = BeautifulSoup(html, 'html.parser')
 
-    # DATES
-    dates_row = soup.find("div", id="columntablejqxgrid").children
-    for cell in dates_row:
-        cell = cell.text
-        if cell:
-            cells_dates.append(cell)
-    # deletes the first column, which doesn't have dates
-    cells_dates.pop(0)
+        cells_operating_expenses = []
+        cells_dates = []
 
-    # making the dictionary
-    net_income = {
-        "net_income":  cells_netIncome[1], "date": cells_dates[0]}
-    return net_income
+        operating_row = soup.find("div", id="row6jqxgrid").children
+        for cell in operating_row:
+            cell = cell.text
+            if cell:
+                cells_operating_expenses.append(cell)
+
+        # DATES
+        dates_row = soup.find("div", id="columntablejqxgrid").children
+        for cell in dates_row:
+            cell = cell.text
+            if cell:
+                cells_dates.append(cell)
+        # deletes the first column, which doesn't have dates
+        cells_dates.pop(0)
+
+        # making the dictionary
+        date = cells_dates[0].replace('-', '/')
+        operating_expenses = clear_number(cells_operating_expenses[1])
+        operating_expenses = {
+            "operating_expenses": operating_expenses, "date": date}
+        return operating_expenses
+
+    except MaxRetryError:
+        pass
 
 
-def filter_num_shares():
+def filter_num_shares(driver, symbol: str, stock_name: str) -> dict:
     # Num Shares
-    cells_num_shares = []
-    cells_dates = []
-    with open('Debug/debug1.txt', 'r') as htmlfile:
-        html = htmlfile.read()
+    try:
 
-    soup = BeautifulSoup(html, 'html.parser')
-    num_shares = soup.find("div", id="row18jqxgrid").children
-    for cell in num_shares:
-        cell = cell.text
-        if cell:
-            cells_num_shares.append(cell)
+        # Getting the full html
+        html = driver.page_source
 
-    # DATES
-    dates_row = soup.find("div", id="columntablejqxgrid").children
-    for cell in dates_row:
-        cell = cell.text
-        if cell:
-            cells_dates.append(cell)
-    # deletes the first column, which doesn't have dates
-    cells_dates.pop(0)
+        cells_num_shares = []
+        cells_dates = []
 
-    # making the dictionary
-    num_shares = {
-        "num_shares":  cells_num_shares[1], "date": cells_dates[0]}
-    return num_shares
+        soup = BeautifulSoup(html, 'html.parser')
+        num_shares = soup.find("div", id="row18jqxgrid").children
+        for cell in num_shares:
+            cell = cell.text
+            if cell:
+                cells_num_shares.append(cell)
+
+        # DATES
+        dates_row = soup.find("div", id="columntablejqxgrid").children
+        for cell in dates_row:
+            cell = cell.text
+            if cell:
+                cells_dates.append(cell)
+
+        # deletes the first column, which doesn't have dates
+        cells_dates.pop(0)
+
+        # making the dictionary
+        date = cells_dates[0].replace('-', '/')
+        operating_expenses = clear_number(cells_num_shares[1])
+        operating_expenses = {
+            "operating_expenses": operating_expenses, "date": date}
+        return operating_expenses
+
+    except MaxRetryError:
+        pass
 
 
-def filter_selling_gen_admin():
+def filter_selling_gen_admin(driver, symbol: str, stock_name: str) -> dict:
     # SG&A
-    cells_S_G_A = []
-    cells_dates = []
+    try:
 
-    soup = BeautifulSoup(html, 'html.parser')
-    sga = soup.find("div", id="row4jqxgrid").children
-    for cell in sga:
-        cell = cell.text
-        if cell:
-            cells_S_G_A.append(cell)
+        # Getting the full html
+        html = driver.page_source
 
-    # DATES
-    dates_row = soup.find("div", id="columntablejqxgrid").children
-    for cell in dates_row:
-        cell = cell.text
-        if cell:
-            cells_dates.append(cell)
-    # deletes the first column, which doesn't have dates
-    cells_dates.pop(0)
+        # SG&A
+        cells_S_G_A = []
+        cells_dates = []
 
-    # making the dictionary
-    sga = {
-        "sga":  cells_S_G_A[1], "date": cells_dates[0]}
+        soup = BeautifulSoup(html, 'html.parser')
+        sga = soup.find("div", id="row4jqxgrid").children
+        for cell in sga:
+            cell = cell.text
+            if cell:
+                cells_S_G_A.append(cell)
 
-    return sga
+        # DATES
+        dates_row = soup.find("div", id="columntablejqxgrid").children
+        for cell in dates_row:
+            cell = cell.text
+            if cell:
+                cells_dates.append(cell)
+
+        # deletes the first column, which doesn't have dates
+        cells_dates.pop(0)
+        # making the dictionary
+        date = cells_dates[0].replace('-', '/')
+        sga = clear_number(cells_S_G_A[1])
+        # making the dictionary
+        sga = {
+            "sga":  sga, "date": date}
+
+        return sga
+    except MaxRetryError:
+        pass
 
 
-def main():
-    # EXCEL Variables:
-    tag = ''
-    complete_tag = ''
+def scraper(symbol: str, stock_name: str):
+    revenue = filter_revenue_TTM(
+        driver=driver, symbol=symbol, stock_name=stock_name)
 
     # Webdriver Settings
     options = ChromeOptions()
     # Sesion without UI
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-javascript")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
@@ -188,15 +215,31 @@ def main():
 
     # Driver sesion initialization
     driver = Chrome(options=options)
-
-    URL = "https://www.macrotrends.net/stocks/charts/AAPL/apple/income-statement?freq=Q"
+    URL = str(
+        f"https://www.macrotrends.net/stocks/charts/%7B{symbol}/{stock_name}/income-statement?freq=Q")
     driver.get(URL)
-    # driver.scroll_from_origin(0, 200)
-    revenue = filter_revenue_TTM(driver)
-    operatin_exp = filter_operating_expenses(driver)
+    driver.implicitly_wait(0)
+
+    revenue = filter_revenue_TTM(
+        driver=driver, symbol=symbol, stock_name=stock_name)
+    operatin_exp = filter_operating_expenses(
+        driver=driver, symbol=symbol, stock_name=stock_name)
+    net_income = filter_net_income(
+        driver=driver, symbol=symbol, stock_name=stock_name)
+    num_shares = filter_num_shares(
+        driver=driver, symbol=symbol, stock_name=stock_name)
+    sga = filter_selling_gen_admin(
+        driver=driver, symbol=symbol, stock_name=stock_name)
+    driver.__del__()
     driver.quit()
-    print(revenue, operatin_exp)
+    finance_variables = {"revenue": revenue,
+                         "operatin_exp": operatin_exp,
+                         "net_income": net_income,
+                         "num_shares": num_shares,
+                         "sga": sga
+                         }
+    return finance_variables
 
 
 if __name__ == '__main__':
-    main()
+    pass
