874ca8b8c65489e2e0ce4508099e12ef45bf74ff
Koch73 <144285829+Koch73@users.noreply.github.com>
2024-09-14 17:41:41 -0300

    Add files via upload

--- formating_tools.py
+++ formating_tools.py
@@ -1,8 +1,20 @@
 import re
 
 
-def clear_number(valor: str) -> float:
-    # Delete simbol '$' and 'B' for formatting
-    match = re.match(r'\$([0-9\.]+)', valor)
-    numero = match.group(1)
-    return float(numero)
+def clear_number(value: str) -> float:
+    """_summary_
+    Delete simbol '$' and 'B' for formatting
+    if the value does not have a simbol, it will return the same value
+
+    Args:
+        value (str): a financial value scraped
+
+    Returns:
+        float: a financial value filtered
+    """
+    try:
+        match = re.match(r'\$([0-9\.]+)', value)
+        numero = match.group(1)
+        return float(numero)
+    except AttributeError:
+        return value
