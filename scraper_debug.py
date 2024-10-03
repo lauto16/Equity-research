a68d146591b7c7dff8cd3d237d6875d4d299acc0
Lautaro Martino <73772193+lauto16@users.noreply.github.com>
2024-10-03 01:17:16 -0300

    Delete scraper_debug.py

--- scraper_debug.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-with open('debug.txt', 'r') as htmlfile:
-    html = htmlfile.read()
-
-soup = BeautifulSoup(html, 'html.parser')
-tables = soup.find_all("table", class_="table")
-print(tables.preatify())
